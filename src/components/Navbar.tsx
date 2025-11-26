57b224c0ea92baae5ec6fef7f6d1e4b1cf76e532
Denny Huang <97718766+DennnnyH@users.noreply.github.com>
2025-11-25 22:59:51 -1000

    Updated navbar & added button to get started
    Changed the our nav brand, added a get started button that leads to the sign up page, fixed the signup and signup page to make them full screen.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable react/jsx-indent, @typescript-eslint/indent */
 'use client';
+
 import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
@@ -10,6 +10,8 @@ import {
   PersonPlusFill,
 } from 'react-bootstrap-icons';
 
+import styles from './navbar.module.css';
+
 const NavBar: React.FC = () => {
   const { data: session } = useSession();
   const currentUser = session?.user?.email;
@@ -17,114 +19,123 @@ const NavBar: React.FC = () => {
   const role = userWithRole?.randomKey;
   const pathName = usePathname();
   return (
-    <Navbar expand="lg" className="uh-navbar">
+    <Navbar expand='lg' className='uh-navbar'>
       <Container
         fluid
-        className="d-flex justify-content-between align-items-center px-3"
+        className='d-flex justify-content-between align-items-center px-3'
       >
-        <Navbar.Brand className="d-flex align-items-center gap-1 px-0">
+        <Navbar.Brand className='d-flex align-items-center gap-1 px-0'>
           <Image
-            src="/uh-manoa-logo.ico"
-            alt="UH Manoa Logo"
+            src='/uh-manoa-logo.ico'
+            alt='UH Manoa Logo'
             width={40}
             height={40}
           />
-          <span className="ms-0">Mānoa Roomie Match</span>
+          <span className={styles.navBrand}>Mānoa 
+            <br />
+            <span className={styles.navBrandLine}>Roomie Match</span></span>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto justify-content-start">
-            <Nav.Link href="/" active={pathName === '/'}>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto justify-content-start'>
+            <Nav.Link href='/' active={pathName === '/'}>
               Home
             </Nav.Link>
-            {!currentUser && (
-              <>
-                <Nav.Link href="/contact" active={pathName === '/contact'}>
-                  Contact Us
-                </Nav.Link>
-                <Nav.Link
-                  href="/how-it-works"
-                  active={pathName === '/how-it-works'}
-                >
-                  How It Works
-                </Nav.Link>
-              </>
-            )}
-
-            {currentUser
+            {!currentUser
               ? [
-                  <Nav.Link
-                    id="find-roommate-nav"
-                    href="/find-roommate"
-                    key="find-roommate"
-                    active={pathName === '/find-roommate'}
-                  >
-                    Find My Roommate
+                  <Nav.Link href='/contact' active={pathName === '/contact'}>
+                    Contact Us
                   </Nav.Link>,
                   <Nav.Link
-                    id="view-roommate-nav"
-                    href="/view-roommates"
-                    key="view-roommates"
+                    href='/how-it-works'
+                    active={pathName === '/how-it-works'}
+                  >
+                    How It Works
+                  </Nav.Link>,
+                ]
+              : ''}
+
+            {currentUser && role === 'USER'
+              ? [
+                  <Nav.Link
+                    id='view-roommate-nav'
+                    href='/view-roommates'
+                    key='view-roommates'
                     active={pathName === '/view-roommates'}
                   >
                     View Roommate Listings
                   </Nav.Link>,
-                  <Nav.Link id="my-messages-nav" href="/messages" key="messages" active={pathName === '/messages'}>
+                  <Nav.Link
+                    id='my-messages-nav'
+                    href='/messages'
+                    key='messages'
+                    active={pathName === '/messages'}
+                  >
                     My Messages
                   </Nav.Link>,
                   <Nav.Link
-                    id="edit-profile-nav"
-                    href="/edit-profile"
-                    key="edit-profile"
+                    id='edit-profile-nav'
+                    href='/edit-profile'
+                    key='edit-profile'
                     active={pathName === '/edit-profile'}
                   >
                     Edit Profile
                   </Nav.Link>,
                 ]
               : ''}
-            {/* {currentUser && role === 'ADMIN' ? (
-              <Nav.Link
-                id="admin-stuff-nav"
-                href="/admin"
-                key="admin"
-                active={pathName === '/admin'}
-              >
-                My Messages
-              </Nav.Link>
-            ) : (
-              ''
-            )} */}
+
+            {/* Admin log in has admin dashboard */}
+            {currentUser && role === 'ADMIN'
+              ? [
+                  <Nav.Link
+                    id='admin-dashboard-nav'
+                    href='/admin/dashboard'
+                    key='admin-dashboard'
+                    active={pathName === '/admin/dashboard'}
+                  >
+                    Admin Dashboard
+                  </Nav.Link>,
+                  <Nav.Link
+                    id='manage-users-nav'
+                    href='/admin/manage-users'
+                    key='manage-users'
+                    active={pathName === '/admin/manage-users'}
+                  >
+                    Manage Users
+                  </Nav.Link>,
+                ]
+              : ''}
           </Nav>
           <Nav>
             {session ? (
-              <NavDropdown id="login-dropdown" title={currentUser}>
+              <NavDropdown id='login-dropdown' title={currentUser} align='end'>
                 <NavDropdown.Item
-                  id="login-dropdown-sign-out"
-                  href="/api/auth/signout"
+                  id='login-dropdown-sign-out'
+                  href='/api/auth/signout'
                 >
                   <BoxArrowRight />
                   Sign Out
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  id="login-dropdown-change-password"
-                  href="/auth/change-password"
+                  id='login-dropdown-change-password'
+                  href='/auth/change-password'
                 >
                   <Lock />
                   Change Password
                 </NavDropdown.Item>
               </NavDropdown>
             ) : (
-              <NavDropdown id="login-dropdown" title="Login" align="end">
+              <NavDropdown id='login-dropdown' title='Login' align='end'>
                 <NavDropdown.Item
-                  id="login-dropdown-sign-in"
-                  href="/auth/signin"
+                  id='login-dropdown-sign-in'
+                  href='/auth/signin'
                 >
                   <PersonFill />
                   Sign in
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  id="login-dropdown-sign-up"
-                  href="/auth/signup"
+                  id='login-dropdown-sign-up'
+                  href='/auth/signup'
                 >
                   <PersonPlusFill />
                   Sign up
